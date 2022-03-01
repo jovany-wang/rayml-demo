8d9614663d7888d8521203ab9697d72303fa0739
Qing Wang <kingchin1218@gmail.com>
2022-03-01 14:23:42 +0800

    REFINE

--- numeral_recognition_case/by_pytorch_with_raytrain.py
+++ numeral_recognition_case/by_pytorch_with_raytrain.py
@@ -44,7 +44,7 @@ def _prepare_train_data():
                                 train = True,
                                 download = True)
 
-    train_loader = DataLoader(train_data, batch_size=64)
+    train_loader = DataLoader(train_data, batch_size=32)
     train_loader = train.torch.prepare_data_loader(train_loader)
     return train_loader    
 
@@ -54,7 +54,7 @@ def _prepare_test_data():
     test_data = datasets.MNIST(root="/tmp/raytrain_demo/data/",
                             transform = transform,
                             train = False)
-    test_loader = torch.utils.data.DataLoader(test_data,batch_size=64,
+    test_loader = torch.utils.data.DataLoader(test_data,batch_size=32,
                                             shuffle=True,num_workers=2)
     return test_loader
 
@@ -64,7 +64,7 @@ class CNN(nn.Module):
         self.conv1 = nn.Conv2d(1,32,kernel_size=3,stride=1,padding=1)
         self.pool = nn.MaxPool2d(2,2)
         self.conv2 = nn.Conv2d(32,64,kernel_size=3,stride=1,padding=1)
-        self.fc1 = nn.Linear(64*7*7,1024) #两个池化，所以是7*7而不是14*14
+        self.fc1 = nn.Linear(64*7*7,1024)
         self.fc2 = nn.Linear(1024,512)
         self.fc3 = nn.Linear(512,10)
 #         self.dp = nn.Dropout(p=0.5)
@@ -73,13 +73,10 @@ class CNN(nn.Module):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
 
-        x = x.view(-1, 64 * 7* 7)#将数据平整为一维的 
+        x = x.view(-1, 64 * 7* 7)
         x = F.relu(self.fc1(x))
-#         x = self.fc3(x)
-#         self.dp(x)
         x = F.relu(self.fc2(x))   
         x = self.fc3(x)  
-#         x = F.log_softmax(x,dim=1) NLLLoss()才需要，交叉熵不需要
         return x
 
 def train_func(config):
@@ -103,19 +100,16 @@ def train_func(config):
 
     for epoch in range(5):
         running_loss = 0.0
-        for i,data in enumerate(train_loader, 0): #0是下标起始位置默认为0
-            # data 的格式[[inputs, labels]]       
+        for i,data in enumerate(train_loader, 0):
             inputs,labels = data
-            # inputs, labels = data[0].to(device), data[1].to(device) # 这一行不work, 108行work
-
-            #初始为0，清除上个batch的梯度信息
+            # Cleanup the data of the last batch
             optimizer.zero_grad()         
-            #前向 + 后向 + 优化     
+            # forward, backward, optimizing
             outputs = net(inputs)
             loss = criterion(outputs,labels)
             loss.backward()
             optimizer.step()
-            # loss 的输出，每个一百个batch输出，平均的loss
+
             running_loss += loss.item()
             if i % 100 == 99:
                 print('[%d,%5d] loss :%.3f' %
@@ -123,14 +117,13 @@ def train_func(config):
                 running_loss = 0.0
             train_loss.append(loss.item())
             
-            # 训练曲线的绘制 一个batch中的准确率
             correct = 0
             total = 0
             _, predicted = torch.max(outputs.data, 1)
-            total = labels.size(0)# labels 的长度
-            correct = (predicted == labels).sum().item() # 预测正确的数目
+            total = labels.size(0)
+            correct = (predicted == labels).sum().item() # Number of the correct predication items.
             train_accs.append(100*correct/total) 
-    # # 绘制出模型效果图
+    # # Draw the graph of the trained model
     # train_iters = range(len(train_accs))
     # _draw_train_process('training',train_iters,train_loss,train_accs,'training loss','training acc')
     print('Finished Training')
@@ -157,7 +150,7 @@ def _load_model_and_predict():
     test_net = CNN()
     test_net.load_state_dict(torch.load("/tmp//raytrain_demo/trainedmodel"))
     test_out = test_net(images)
-    # 输出的是每一类的对应概率，所以需要选择max来确定最终输出的类别 dim=1 表示选择行的最大索引
+
     print(test_out)
     _, predicted = torch.max(test_out, dim=1)
     print('Predicted: ', ' '.join('%d' % predicted[j]
