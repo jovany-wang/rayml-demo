10e1ac9bbebf18a26b0f8de865dd7ca3a2269176
Qing Wang <kingchin1218@gmail.com>
2022-04-26 13:25:42 +0800

    Add send/recv benchmark.

---
+++ collective/benchmark/send_recv_case.py
@@ -0,0 +1,88 @@
+import argparse
+
+import ray
+import ray.util.collective as col
+import numpy as np
+import time
+
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+parser = argparse.ArgumentParser(
+    description=("None")
+)
+
+parser.add_argument(
+    "--num-rounds",
+    type=int,
+    help="",
+)
+
+args, remaining_args = parser.parse_known_args()
+
+
+@ray.remote(num_cpus=1)
+class Sender:
+    def __init__(self):
+        self.send_buf = np.array([[1,2,3],[4,5,6]], dtype=np.float32)
+        col.init_collective_group(2, 0, backend="gloo", group_name="default")
+
+    def do_send(self):
+        start_time = time.time()
+        logger.warning("Starting to send num_round(%s) at time %s", num_rounds, start_time)
+        for _ in range(0, num_rounds):
+          col.send(self.send_buf, 1, group_name="default")
+        end_time = time.time()
+        logger.warning("End to send at time %s", end_time)
+        return True
+
+    def destroy(self):
+        col.destroy_group()
+
+    def ping(self):
+        return "pong"
+
+
+@ray.remote(num_cpus=1)
+class Recver:
+    def __init__(self):
+        np.zeros((2, 3), dtype=np.float32)
+        col.init_collective_group(2, 1, backend="gloo", group_name="default")
+
+    def do_recv(self, num_rounds: int):
+        start_time = time.time()
+        logger.warning("Starting to recv  num_round(%s) at time %s", num_rounds, start_time)
+        for _ in range(0, num_rounds):
+          col.recv(self.recv_buf, 0, group_name="default")
+        end_time = time.time()
+        logger.warning("End to recv at time %s", end_time)
+        return True
+
+    def destroy(self):
+        col.destroy_group()
+
+    def ping(self):
+        return "pong"
+
+
+def main(num_rounds: int):
+    ray.init(num_cpus=6)
+    sender = Sender.remote()
+    recver = Recver.remote()
+
+    ray.get(sender.ping.remote())
+    ray.get(recver.ping.remote())
+
+    send_obj = sender.do_send.remote(num_rounds)
+    recv_obj = recver.do_recv.remote(num_rounds)
+
+    print("send_buf:", ray.get(send_obj))
+    print("recv_buf:", ray.get(recv_obj))
+    print("==========END")
+
+
+if __name__ == "__main__":
+    num_rounds = args.num_rounds
+    main(num_rounds)
